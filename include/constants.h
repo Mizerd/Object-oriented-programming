1c205c764a16e55c44dd7e04e51818c66238a544
Mizerd <155391761+Mizerd@users.noreply.github.com>
2026-03-10 15:22:21 +0200

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -4,6 +4,6 @@ namespace cfg {
 inline constexpr double kDefaultNdWeight = 0.4;
 inline constexpr double kDefaultEgzWeight = 0.6;
 
-inline constexpr int kMaxStudentsGenerate = 10000;
+inline constexpr int kMaxStudentsGenerate = 10000000;  // allow up to 10M
 inline constexpr int kMaxNdPerStudent     = 1000;
 }
