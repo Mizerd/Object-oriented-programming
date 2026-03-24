601d658743ffdd49c1fb6ebc6d8798cd2ee0b471
Mizerd <155391761+Mizerd@users.noreply.github.com>
2026-03-24 13:05:43 +0200

    Update fileio.h

--- include/fileio.h
+++ include/fileio.h
@@ -1,17 +1,9 @@
 #pragma once
 
-#include "types.h"
+#include "student_container.h"
 
-#include <cstddef>
 #include <string>
-#include <vector>
 
-// Nuskaitymas iš failo (v0.2 funkcionalumas). Meta std::runtime_error, jei:
-//  - failas neegzistuoja / neatsidaro
-//  - neteisinga antraštė ar formatas
-//  - trūksta pažymių
-void readStudentsFromFileVec(const std::string& filename,
-                             std::vector<StudentRec>& out,
-                             std::size_t& maxVCols,
-                             std::size_t& maxPCols,
-                             const Weights& weights);
+void readStudentsFromFile(const std::string& filename,
+                          StudentContainer& out,
+                          const Weights& weights);
